ce173739c6e2b45ed403942876c02bc55940f643
Richard O'Dwyer <richard@richard.do>
2018-05-18 10:14:43 +0100

    wip

--- tests/test_brickftp.py
+++ tests/test_brickftp.py
@@ -23,10 +23,23 @@ class TestThing:
             password=BRICK_FTP_PASS,
             subdomain=BRICK_FTP_SUBDOMAIN,
         )
+        yield client
         paths = (i['path'] for i in client.dir(BASE_DIR))
         for path in paths:
             client.delete(path)
-        yield client
 
-    def test_something(self):
-        pass
+    def test_thing(self, brickftp):
+        # create subdir
+        remote_subdir_path = Path(BASE_DIR, str(uuid4()))
+        brickftp.mkdir(remote_subdir_path)
+        # check it worked
+        brickftp.dir(remote_subdir_path)
+        # upload a file to it
+        remote_subsubdir_path = str(Path(remote_subdir_path, str(uuid4())))
+        brickftp.mkdir(remote_subsubdir_path)
+        filename = f'my_test_file{uuid4()}'
+        brickftp.upload(
+            upload_path=Path(remote_subsubdir_path, filename),
+            local_path=Path(__file__),
+            encoding='latin-1'
+        )
